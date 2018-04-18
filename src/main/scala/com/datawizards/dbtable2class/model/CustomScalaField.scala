7fd763f0e90694c3a9cb75421c5c4bc4847f05b3
Mateusz Boryn <mateusz.boryn@stepstone.com>
2018-04-18 10:51:15 +0200

    line endings

--- src/main/scala/com/datawizards/dbtable2class/model/CustomScalaField.scala
+++ src/main/scala/com/datawizards/dbtable2class/model/CustomScalaField.scala
@@ -5,5 +5,7 @@ case class CustomScalaField (
                             fieldType: String,
                             fieldAnnotations: List[String] = List()
                             ) {
+
   def fieldToString(indentWidth: Int = 2) : String = fieldAnnotations.map(_ + "\n" + " "*indentWidth).mkString + s"$fieldName : $fieldType"
+
 }
